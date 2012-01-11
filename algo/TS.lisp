97d82f96ef1132e04a0a3f4ac3c51bfc02987a66
Marc Kuo <kuomarc2@gmail.com>
2012-01-10 21:49:51 -0500

    added candidate lists to tabu-search - cleaned up code

--- algo/TS.lisp
+++ algo/TS.lisp
@@ -102,27 +102,24 @@
 	(iter moves))))
 
 ;; --------------------
-;; For each TS iteration:
-;; Select move from candidate-list. if there is none:
+;; If there is no candidate-list
 ;;   generate-assess-sort moves
-;;   if top move is an improving one
-;;     create a candidate list and perform top move
-;;     otherwise, select first move not on tabu-list
+;;   create a candidate list and perform top move
+;; Perform top move from candidate-list
 
 (defmethod iterate ((ts tabu-search))
   (let ((sol (algo-current-sol ts)))
     (labels ((perform-add-tabu (move)
+	       "add move to tabu-list and perform it"
 	       (add-to-tabu ts move)
 	       (perform-move sol move))
 	     (select-perform-from-cand (ts)
+	       "select best move from candidate-list, remove all related moves and perform"
 	       (let ((best-move (car (tabu-search-candidate-list ts))))
-		 (when (null best-move) (error "No move selected!"))
-		 (remove-affected-moves ts best-move) ;remove related moves from cand list
+		 (remove-affected-moves ts best-move) 
 		 (perform-add-tabu best-move))))
-      (aif (tabu-search-candidate-list ts) ;if there is a candidate list, select move from it
+      (aif (tabu-search-candidate-list ts) 
 	   (select-perform-from-cand ts)
-					;otherwise generate-assess-sort moves
 	   (let ((sorted-moves (sort-moves (assess-moves sol (generate-moves ts)))))
-	     (princ "no cand list")
 	     (setf (tabu-search-candidate-list ts) (create-candidate-list ts sorted-moves))
 	     (select-perform-from-cand ts))))))
