ccb63f87730ec40adc9eb8022e6fd8f81f6d106b
Marc Kuo <kuomarc2@gmail.com>
2012-01-16 14:24:21 -0500

    split get-closest-vehicle and get-closest-feasible-vehicle

--- algo/tools.lisp
+++ algo/tools.lisp
@@ -9,13 +9,17 @@
   (:method (node problem) "get-closest-vehicle: Expects <node> and <problem> inputs!")
   (:documentation "Returns the closest <vehicle> to <node>. Used by insertion heuristic. When multiple <vehicle> are on equal distance, choose first one (i.e. lowest ID)."))
  
-(defmethod get-closest-vehicle ((n node) (prob problem))
+(defmethod get-closest-vehicle ((n node) (prob TSP))
   (let ((distances (mapcar #'(lambda (x) (node-distance (last-node x) n))
 			   (fleet-vehicles (problem-fleet prob))))) ;all distances
     (vehicle prob (get-min-index distances))))
 
+
 ;; allow only feasible vehicles to be selected
-(defmethod get-closest-vehicle ((n node) (prob CVRP))
+(defmethod get-closest-feasible-vehicle ((n node) (prob TSP))
+  (get-closest-vehicle n prob))
+
+(defmethod get-closest-feasible-vehicle ((n node) (prob CVRP))
   (let* ((vehicles (fleet-vehicles (problem-fleet prob)))
 	 (dists (mapcar #'(lambda (x) (node-distance (last-node x) n)) vehicles))
 	 (caps (mapcar #'(lambda (x)
@@ -27,19 +31,25 @@
 			   dists caps)))
     (vehicle prob (get-min-index filtered))))	   
 
-(defmethod get-closest-vehicle ((n node) (prob VRPTW))
+;; for VRPTW, consider both capacity and time. Feasiblility of appending at the end only.
+(defmethod get-closest-feasible-vehicle ((n node) (prob VRPTW))
   (let* ((vehicles (fleet-vehicles (problem-fleet prob)))
 	 (dists (mapcar #'(lambda (x) (node-distance (last-node x) n)) vehicles))
 	 (times (mapcar #'(lambda (x)
 			    (multiple-value-bind (c time)
 				(in-timep x (problem-network prob)) (when c time)))
 			vehicles))
+	 (caps (mapcar #'(lambda (x)
+			   (multiple-value-bind (c cap)
+			       (in-capacityp x) (when c cap)))
+		       vehicles))
 	 (filtered (mapcar #'(lambda (veh dist time)
-			       (if (> (+ time (travel-time veh
-							   (problem-network prob)
-							   (node-id (last-node veh))
-							   (node-id n)))
-				      (node-end n))
+			       (if (or (> (node-demand n) cap)
+				       (> (+ time (travel-time veh
+							       (problem-network prob)
+							       (node-id (last-node veh))
+							       (node-id n)))
+					  (node-end n)))
 				   nil
 				   dist))
 			   vehicles dists times)))
