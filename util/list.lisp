8ba2ad8ddc32ab3bc17580d25312be3a99d9a537
Marc Kuo <kuomarc2@gmail.com>
2012-01-30 15:01:43 -0500

    deleted unused obsolete functions

--- util/list.lisp
+++ util/list.lisp
@@ -1,9 +1,9 @@
-;;; Updated Tue Oct 25, 2011 (c) Marc Kuo
-;;; ------------------------
-;;; Simple Util for list manipulation. Used on simple routes, i.e. permutation of numbers that represent nodes.
+;;; Simple Util for list manipulation. Basic tools used by route.lisp mostly.
+
 (in-package :open-vrp.util)
 
 ;; Simple list utils
+;; --------------------
 (defun get-from-list (list pred &optional ans)
   "Gets from list the value (max or min) while ignoring NIL's. Returns NIL if the whole list is nil. Use get-min or get-max!"
   (if (null list) (if (numberp ans) ans nil)
@@ -47,9 +47,12 @@
 	     ,predicate
 	     :key #'(lambda (x) (or ,(if key `(funcall ,key x) `x)
 				    ,ignore))))))
+
+;; --------------------------
   
 ;; Single route
-;; Sun Dec 11, 2011 - Adjusted to not allow for index-out-of-bounds
+;; -------------------------
+
 (defun insert-before (object index list)
   "Insert object before index of list. 0 implies inserting in front, length of list implies appending at the end. Throws index out of bounds when index is larger."
   (labels ((iter (obj i ls)
@@ -79,47 +82,17 @@
 				   (cdr lst))))))
 	  (values (iter index list) item)))))
 
-(defmacro apply-on-index (list index function)
-  "Macro to apply function only on index in list. NON-destructive. Accepts a function with one argument which will use as input the nth of the list. Returns a list."
-  `(if (>= ,index (length ,list)) (error "Index is larger than index!")
-       (labels ((iter-fn (i rest)
-		  (if (= -1 i) rest
-		      (cons (if (= 0 i) (funcall ,function (car rest)) (car rest))
-			    (iter-fn (1- i) (cdr rest))))))
-	 (iter-fn ,index ,list))))
-
 (defun mark-nill (list indices)
   "Marks the indices on list with NIL. DESTRUCTIVE."
   (mapcar #'(lambda (x) (setf (nth x list) nil)) indices)
   list)
-
-(defun mark-nill-items (list items)
-  "Marks the items on list with NIL."
-  (labels ((iter (rest-items ans)
-	     (if (null rest-items) ans
-		 (iter (cdr rest-items)
-		       (substitute nil (car rest-items) ans)))))
-    (iter items list)))
    
 (defmacro with-tabu-indices (tabu-indices fn arg-list)
   `(funcall ,fn (mark-nill (copy-list ,arg-list) ,tabu-indices)))
 
-(defmacro with-tabu-items (tabu-items fn arg-list)
-  `(funcall ,fn (mark-nill-items (copy-list ,arg-list) ,tabu-items)))
-
 (defun enumerate-interval (n)
   "Returns a list from 1 to n."
   (map1-n #'(lambda (x) x) n))
-
-(defun shuffle-pool (pool)
-  "Given a pool, shuffle the pool and return a new pool. Undestructive."
-  (if (null pool)
-      nil
-      (let* ((len (length pool))
-            (x (random len))
-            (val (nth x pool)))
-       (cons val
-             (shuffle-pool (remove val pool))))))
 
 (defun random-list-permutation (length)
   "Randomly creates a permutation from 1 to length."
