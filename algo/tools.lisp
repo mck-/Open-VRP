19c12107d80e892b4bbdf7cd812cda175e65319a
Marc Kuo <kuomarc2@gmail.com>
2012-01-30 17:02:29 -0500

    removed unnecessary defmethods, using defun instead and some bugfixes

--- algo/tools.lisp
+++ algo/tools.lisp
@@ -14,7 +14,7 @@
   "Returns index of the first next closest, that is not in chosen (which is a list)."
   (with-tabu-indices tabu #'get-min-index distances))
 
-(defun get-closest-node ((prob problem) veh-id &optional tabu)
+(defun get-closest-node (prob veh-id &optional tabu)
   "Returns the closest node from the last location of vehicle. Requires <problem> and vehicle-ID. A tabu list of node-IDs is optional to exclude consideration of some nodes."
   (let* ((loc (last-node (vehicle prob veh-id)))
 	 (dists (get-array-row (problem-dist-array prob) (node-id loc))))
@@ -25,17 +25,13 @@
 ;; Closest Vehicle
 ;; ---------------------------
 ;; challenge: what if the vehicle is located on the node n - use only for initial insertion?
-(defgeneric get-closest-vehicle (node problem)
-  (:method (node problem) "get-closest-vehicle: Expects <node> and <problem> inputs!")
-  (:documentation "Returns the closest <vehicle> to <node>. Used by insertion heuristic. When multiple <vehicle> are on equal distance, choose first one (i.e. lowest ID)."))
- 
-(defmethod get-closest-vehicle ((n node) (prob TSP))
+(defun get-closest-vehicle (n prob)
+  "Returns the closest <vehicle> to <node>. Used by insertion heuristic. When multiple <vehicle> are on equal distance, choose first one (i.e. lowest ID)."
   (let ((dists (mapcar #'(lambda (x) (node-distance (last-node x) n)) (problem-fleet prob))))
     (vehicle prob (get-min-index dists))))
 
-
-;; allow only feasible vehicles to be selected
-(defmethod get-closest-feasible-vehicle ((n node) (prob TSP))
+;; challenge: below code can be improved by re-using constraints checker funcs in some way.
+(defmethod get-closest-feasible-vehicle ((n node) (prob problem))
   (get-closest-vehicle n prob))
 
 (defmethod get-closest-feasible-vehicle ((n node) (prob CVRP))
@@ -78,9 +74,9 @@
 
 (defgeneric optimal-insertion (prob node)
   (:method (prob node) "optimal-insertion: Expects <Problem> and <Node>.")
-  (:documentation "Given a node and a solution (that does not have this node yet), insert the node in the best possible and feasible location."))
+  (:documentation "Given a node and a solution (that does not have this node yet), insert the node in the best possible and feasible location. DESTRUCTIVE."))
 
-(defmethod optimal-insertion ((sol tsp) (n node))
+(defmethod optimal-insertion ((sol problem) (n node))
   (labels ((iter (flt best-move)
 	     (if (null flt) best-move
 		 (iter (cdr flt)
