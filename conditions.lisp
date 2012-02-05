38e13b87924deba69ef79a74f7ceb392ee2951f9
Marc Kuo <kuomarc2@gmail.com>
2012-02-05 12:17:40 -0500

    added conditions to constraints checkers, to make sure the vehicle has its required parameters initialized

--- conditions.lisp
+++ conditions.lisp
@@ -5,7 +5,8 @@
 
 ;; util/lists.lisp
 (define-condition unaccepted-predicate (error)
-  ((pred :initarg :pred :reader pred)))
+  ((pred :initarg :pred :reader pred))
+  (:report "Accepts only #'> or #'<."))
 
 (define-condition index-out-of-bounds (error)
   ((index :initarg :index :reader index)
@@ -14,8 +15,17 @@
 ;; constraints.lisp
 (define-condition infeasible-solution (error)
   ((sol :initarg :sol :reader sol)
-   (func :initarg :func :reader func)
-   (msg :initarg :msg :reader msg)))
+   (func :initarg :func :reader func))
+  (:report "The provided solution is already infeasible, cannot check for feasibility of the move."))
+
+(define-condition no-capacities-vehicle (error)
+  ((veh :initarg :veh :reader veh))
+  (:report "Trying to check capacities for a vehicle that has no defined capacity."))
+
+(define-condition no-speed-vehicle (error)
+  ((veh :initarg :veh :reader veh))
+  (:report "Trying to check TW constraints for a vehicle that has no defined speed."))
+   
 
 ;; algo/TS.lisp
 (define-condition all-moves-tabu (error)
