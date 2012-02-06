27bb2bd1d1fb01c630e618002e1a12a0fc2ec52a
Marc Kuo <kuomarc2@gmail.com>
2012-02-06 18:06:37 -0500

    adjusted constraintsp to use method-combination - adjusted constraints-check to optionally provide endtest

--- util/constraints.lisp
+++ util/constraints.lisp
@@ -10,28 +10,22 @@
 ;; ----------------------------
 
 (defgeneric constraintsp (prob)
-  (:method (obj) "constraintsp: Expects <Problem> object!")
-  (:documentation "Tests weather the solution in the <problem> is complying with the constraints. If the problem is a CVRP, check for capacity. If it is a VRPTW, check for capacity and time-windows."))
+  (:documentation "Tests weather the solution in the <problem> is complying with the constraints. If the problem is a CVRP, check for capacity. If it is a VRPTW, check for time-windows. For CVRPTW, that inherits from both classes, check both constraints.")
+  (:method-combination and))
 
-(defmethod constraintsp ((prob problem))
-  T)
+(defmethod constraintsp and ((prob problem)) T)
 
-(defmethod constraintsp :around ((sol CVRP))
-  (if (in-capacityp sol)
-      (call-next-method)
-      NIL))
+(defmethod constraintsp and ((sol CVRP)) (in-capacityp sol))
 
-(defmethod constraintsp :around ((sol VRPTW))
-  (if (in-timep (problem-fleet sol))
-      (call-next-method)
-      NIL))
+(defmethod constraintsp and ((sol VRPTW)) (in-timep (problem-fleet sol)))
 
 ;; Helper macro for defining constraints-checking methods below
 ;; Returns NIL as soon as it finds out that a constraint is violated
-(defmacro constraints-check (arglist init-forms next-forms testform endtest)
+(defmacro constraints-check (arglist init-forms next-forms testform &optional endtest)
   (let ((iter (gensym)))
     `(labels ((,iter ,arglist
-		(if ,endtest (values T ,@(cdr arglist))
+		(if ,@(if endtest `(,endtest) `((null ,(car arglist))))
+		    (values T ,@(cdr arglist))
 		    (and
 		     ,testform
 		     (,iter ,@next-forms)))))
@@ -41,7 +35,7 @@
 ;; 1. Capacity Constraints
 ;; ------------------------
 
-(defgeneric in-capacityp (veh/problem)
+(defgeneric in-capacityp (veh/cvrp)
   (:method (obj) "Expects a <Vehicle>/<CVRP> object!")
   (:documentation "Tests weather the route on <vehicle> is complying with the capacity constraint. Returns T and the remaining capacity if it does. When <CVRP> is provided, test all vehicles."))
 
@@ -51,23 +45,15 @@
    (route cap)
    ((vehicle-route v) (vehicle-capacity v))
    ((cdr route) (- cap (node-demand (car route))))
-   (<= (node-demand (car route)))
-   (null route)))
+   (<= (node-demand (car route)) cap)))
 
 (defmethod in-capacityp ((pr CVRP))
   (constraints-check
    (flt)
    ((problem-fleet pr))
    ((cdr flt))
-   (in-capacityp (car flt))
-   (null flt)))
+   (in-capacityp (car flt))))
 	   
-(defun node-fit-in-vehiclep (sol node-id vehicle-id)
-  "Helper function for assessing constraints. Used by assess-move :around."
-  (multiple-value-bind (comply cap-left) (in-capacityp (vehicle sol vehicle-id))
-    (if (not comply) (error 'infeasible-solution :sol sol :func #'in-capacityp)
-	(<= (node-demand (node sol node-id)) cap-left))))
-
 ;; ------------------------------
 
 ;; 2. Time-window constraints
@@ -79,7 +65,6 @@
       (/ (node-distance n1 n2) speed)
     (same-origin-destination () 0)))
 	 
-
 (defun time-after-serving-node (node arrival-time)
   "Given a node to serve and the current time, return the new time (if on-time to begin with). When arrival-time is too early, wait till earliest start time."
   (cond ((> arrival-time (node-end node)) (error 'infeasible-solution :sol node :func arrival-time :msg "Arrival time is later than latest start-time of node"))
@@ -90,13 +75,6 @@
   (:method (veh/vrptw) "in-timep: Expects <Vehicle-TW>/<Problem>!")
   (:documentation "Tests weather the route on <Vehicle> is complying with the time-window constraints. Returns T and the time of finishing its last task. When <Fleet> is provided, test all vehicles."))
 
-;; check for each node
-;; arrival time is current time + travel time
-;; if arrival time < end-time, on time (if not on time, return NIL - no need to check more)
-;;   if arrival time < begin-time, wait until begin time -> set time to begin + duration
-;;   if arrival after begin-time -> set time to arrival + duration
-
-;; check for one vehicle
 (defmethod in-timep ((v vehicle))
   (unless (vehicle-speed v) (error 'no-speed-vehicle :veh v))
   (symbol-macrolet ((to (car route))
@@ -105,17 +83,22 @@
      (route time loc)
      ((cdr (vehicle-route v)) 0 (car (vehicle-route v)))
      ((cdr route) (time-after-serving-node to arr-time) to)
-     (<= arr-time (node-end to))
-     (null route))))
+     (<= arr-time (node-end to)))))
     
-;; check for whole fleet
 (defmethod in-timep ((pr VRPTW))
   (constraints-check
    (veh)
    ((problem-fleet pr))
    ((cdr veh))
-   (in-timep (car veh))
-   (null veh)))
-		  
-		 
+   (in-timep (car veh))))
 ;; -------------------------
+
+;; Feasibility check of insertion
+;; ----------------------------
+
+(defun node-fit-in-vehiclep (sol node-id vehicle-id)
+  "Helper function for assessing constraints. Used by assess-move :around."
+  (multiple-value-bind (comply cap-left) (in-capacityp (vehicle sol vehicle-id))
+    (if (not comply) (error 'infeasible-solution :sol sol :func #'in-capacityp)
+	(<= (node-demand (node sol node-id)) cap-left))))
+
