4e35fed187725cff41a137c2581a5b6232c91e35
Marc Kuo <kuomarc2@gmail.com>
2012-02-06 18:05:43 -0500

    removed obsolete methods

--- util/fitness.lisp
+++ util/fitness.lisp
@@ -8,14 +8,6 @@
   (:documentation "The generic fitness function. To be defined for each class of <problem> specifically. This function allows for custom fitness-functions for your own defined <problem> classess. The default fitness function is total distance."))
 
 (defmethod fitness ((prob problem))
-  (total-dist prob (problem-dist-array prob)))
-
-(defmethod fitness ((cvrp CVRP))
   (values
-   (call-next-method)
-   (constraintsp cvrp)))
-
-(defmethod fitness ((vrptw VRPTW))
-  (values
-   (call-next-method)
-   (constraintsp vrptw)))
+   (total-dist prob (problem-dist-array prob))
+   (constraintsp prob)))
