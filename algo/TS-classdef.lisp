e8527240d28bf258410f40103289bdc176cbf120
Marc Kuo <kuomarc2@gmail.com>
2012-02-21 14:00:14 -0500

    amalgamated TS and Tabu list objects into one

--- algo/TS-classdef.lisp
+++ algo/TS-classdef.lisp
@@ -1,30 +1,24 @@
-;; Tabu Search - added Mon Dec 12, 2011
+;; Tabu Search Classes
 ;; ---------------------------------------
 ;; - it is possible to define your own moves and initial solution heuristic.
 (in-package :open-vrp.algo)
 
 (defclass tabu-search (algo)
   ((name :initform "Tabu Search")
-   (desc :initform "Simple Tabu Search heuristic, see (Kuo 2011) for details.")
-   (moves :accessor tabu-search-moves :initarg :moves :initform 'TS-best-insertion-move)
-   (init-heur :accessor tabu-search-init-heur :initarg :init-heur :initform 'greedy-best-insertion)
-   (iterations :initform 5)
-   (animate :accessor tabu-search-animate :initarg :animate :initform nil)
-   (tabu-list :accessor tabu-search-tabu-list :initarg :tabu-list :initform (make-instance 'tabu-list))
-   (candidate-list :accessor tabu-search-candidate-list :initarg :candidate-list :initform nil)))
+   (desc :initform "A local search that escapes local optima by means of declaring certain moves tabu.")
+   (move-type :accessor ts-move-type :initarg :move-type :initform 'TS-best-insertion-move)
+   (init-heur :accessor ts-init-heur :initarg :init-heur :initform 'greedy-best-insertion)
+   (iterations :initform 20)
+   (animate :accessor ts-animate :initarg :animate :initform nil)
+   (aspiration :accessor ts-aspiration :initarg :aspiration :initform T)
+   (elite-list :accessor ts-elite-list :initarg :elite-list :initform T)
+   (tabu-list :accessor ts-tabu-list :initarg :tabu-list)
+   (tabu-tenure :accessor ts-tenure :initarg :tabu-tenure)
+   (candidate-list :accessor ts-candidate-list :initarg :candidate-list :initform nil)))
 
 ;; ----------------------------
 
-;; Tabu List
-;; -----------------------------
-(defclass tabu-list ()
-  ((tabu :accessor tabu-list-tabu :initarg :tabu :initform nil)
-   (tenure :accessor tabu-list-tenure :initarg :tenure :initform 25)
-   (aspiration :accessor tabu-list-aspiration :initarg :aspiration :initform T)))
-
-;; ---------------------------
-
-;; Tabu search move
+;; Tabu search move types
 ;; ---------------------------
 
 ;; Given a node and a vehicle, TS-best-insertion inserts it in the best possible position
