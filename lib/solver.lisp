827580d26fe4e3d07906e4dd9e5082f1c3dd44b9
Marc Kuo <kuomarc2@gmail.com>
2012-03-17 17:49:18 -0400

    bugfix for multi-run

--- lib/solver.lisp
+++ lib/solver.lisp
@@ -83,8 +83,8 @@
   `(progn
      ;so it won't override the first log-file made by solve-prob use -1 seconds
      (setq *multi-run-start-time* (- (get-universal-time) 1))
-     (loop for ,(gensym) below ,times collect ,@algo-call)
-     (setq *multi-run-finish-time* (get-universal-time))))
+     (loop for ,(gensym) below ,times collect ,@algo-call
+	finally (setq *multi-run-finish-time* (get-universal-time)))))
   
 (defun get-best-solution-from-multi-run (solutions)
   "Given a list of solutions (from multi-run), return the best solution."
