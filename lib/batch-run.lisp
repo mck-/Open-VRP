621babe7447bcc6c7c868153adf4a539a792be21
Marc Kuo <kuomarc2@gmail.com>
2012-03-29 16:32:08 +0200

    adjusted example to use test batch dir

--- lib/batch-run.lisp
+++ lib/batch-run.lisp
@@ -6,8 +6,8 @@
 (defmacro batch-run ((x dir-path loader-fn &key (plotp nil) (log-mode 1)) output-file num-times &body algo-call)
   "Given a directory, will call algo-call on each file that is loaded using the loader-fn and bound to x. Output-file is a mandatory filepath to which the results will be written. Algo-call must return an <Algo> object (e.g. multi-run-algo or solve-prob). Num-times is the number of times the algo-call will run on each test-case, which will be used for stats.
 
-Example: (batch-run (test-case \"~/VRP-benchmarks/solomon-100/\" #'load-solomon-vrp-file)
-	      \"~/CL/Open-VRP/run-logs/batch-test.txt\" 20
+Example: (batch-run (test-case \"test-cases/Solomon-25/\" #'load-solomon-vrp-file)
+	      \"run-logs/Solomon-25-batch.txt\" 20
        (solve-prob test-case (make-instance 'tabu-search :iterations 300)))."
   (with-gensyms (file str results)
     `(progn
