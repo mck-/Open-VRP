efc5bb8490882f1a0b8c3dfa112d47aa98717ec9
Marc Kuo <kuomarc2@gmail.com>
2013-09-30 15:08:35 -0700

    add unit tests for get-min/max-index

--- test/util-list.lisp
+++ test/util-list.lisp
@@ -18,3 +18,23 @@
   (assert-equal 3 (get-max '(0 nil 3 -5 1)))
   (assert-error 'list-of-nils (get-max '(nil nil nil)))
   (assert-equal 203948 (get-max '(nil 10 nil 203948 3 1))))
+
+(define-test get-min-index
+  "Test the get-min-index util"
+  (:tag :util)
+  (assert-equal 3 (get-min-index '(0 2 3 -5 1)))
+  (assert-equal 3 (get-min-index '(0 nil 3 -5 1)))
+  (assert-error 'list-of-nils (get-min-index '(nil nil nil)))
+  (assert-equal '(5 1) (multiple-value-bind (i val)
+                           (get-min-index '(nil 10 nil 203948 3 1))
+                         (list i val))))
+
+(define-test get-max-index
+  "Test the get-max-index util"
+  (:tag :util)
+  (assert-equal 2 (get-max-index '(0 2 3 -5 1)))
+  (assert-equal 2 (get-max-index '(0 nil 3 -5 1)))
+  (assert-error 'list-of-nils (get-max-index '(nil nil nil)))
+  (assert-equal '(3 203948) (multiple-value-bind (i val)
+                                (get-max-index '(nil 10 nil 203948 3 1))
+                              (list i val))))
