d4cce81e8f8c3643f45382f843d2dc63008ba0f0
Marc Kuo <kuomarc2@gmail.com>
2013-10-01 21:52:39 -0700

    move benchmark-related functions to benchmarks in test folder, outside of the lib package

--- lib/network.lisp
+++ lib/network.lisp
@@ -39,33 +39,7 @@
         (gethash to row)
         (error 'expect-hash-table :fun 'distance :arg row))))
 
-(defun distance-coords (x1 y1 x2 y2)
-  "Calculates pythagoras distance"
-  (flet ((square (x)
-           (* x x)))
-    (sqrt (+ (square (- x1 x2)) (square (- y1 y2))))))
-
-(defun distance-coord-pair (n1 n2)
-  "Calculates distance given two coord pairs. Returns NIL if both coords are the same."
-  (if (eql n1 n2)
-      NIL
-      (distance-coords (car n1) (cdr n1)
-                       (car n2) (cdr n2))))
-
-(defun generate-dist-array (coord-list)
-  "Given a list of coord pairs, generate an array of distances."
-  (let* ((size (length coord-list))
-         (dist-array (eval `(make-array '(,size ,size) :initial-element nil))))
-    (map0-n #'(lambda (x)
-                (map0-n #'(lambda (y)
-                            (setf (aref dist-array x y)
-                                  (distance-coord-pair (nth x coord-list)
-                                                       (nth y coord-list))))
-                        (1- size)))
-            (1- size))
-    dist-array))
-
-;; ----------------------------------------
+;; -------------------------
 
 ;; Accessor functions
 ;;--------------------------
@@ -73,10 +47,11 @@
 (defmethod node ((prob problem) id)
   (aref (problem-network prob) id))
 
-;; --------------------------------
+;; -------------------------
 
 ;; Create Node macro
-;; ------------------
+;; -------------------------
+
 (defmacro new-node (id xcor ycor &key demand start end duration)
   `(make-node :id ,id :xcor ,xcor :ycor ,ycor
               ,@(when demand `(:demand ,demand))
