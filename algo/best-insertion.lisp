8ebca95c9d93ca4396bf4f7798b66cfa85ca49b8
Marc Kuo <kuomarc2@gmail.com>
2013-10-08 15:45:52 -0700

    fix indentation for best-insertion.lisp

--- algo/best-insertion.lisp
+++ algo/best-insertion.lisp
@@ -2,23 +2,23 @@
 ;; -------------
 ;; The following functions are defined to generate-assess-choose the best insertion move
 ;; for a node into a vehicle's route. Assumes the route does not include the node already.
-;; - get-best-insertion-move expects a <solution> object, a node-id and a vehicle-id. 
+;; - get-best-insertion-move expects a <solution> object, a node-id and a vehicle-id.
 ;; -----------------------------------------------
 (in-package :open-vrp.algo)
 
 (defun generate-insertion-moves (sol vehicle-id node-id)
   "Given the <solution> object, vehicle-id and node-id (integers), create all possible insertion-moves, and return them in a list. Avoid generating moves that won't do anything (when doing intra-route insertion)."
   (let* ((route (vehicle-route (vehicle sol vehicle-id)))
-	 (pos (position node-id route :key #'node-id)) ;check if we do intra-route insertion
-	 (moves '()))
+         (pos (position node-id route :key #'node-id)) ;check if we do intra-route insertion
+         (moves '()))
     (do ((index 1 (1+ index)))
-	((> index (if (problem-to-depot sol) (1- (length route)) (length route))))
+        ((> index (if (problem-to-depot sol) (1- (length route)) (length route))))
       (unless (and pos (or (= index pos) (= index (1+ pos)))) ;useless moves avoided
-	(push (make-insertion-move
-	       :index index
-	       :vehicle-id vehicle-id
-	       :node-id node-id)
-	      moves)))
+        (push (make-insertion-move
+               :index index
+               :vehicle-id vehicle-id
+               :node-id node-id)
+              moves)))
     (nreverse moves)))
 
 ;; calculates the added distance of performing the insertion-move
@@ -27,18 +27,18 @@
 (defmethod assess-move ((sol problem) (m insertion-move))
   (with-slots (node-ID vehicle-ID index) m
     (let* ((route (route-to m sol))
-	   (dist-array (problem-dist-array sol))
-	   (node-before (node-id (nth (1- index) route))))
+           (dist-array (problem-dist-array sol))
+           (node-before (node-id (nth (1- index) route))))
       (setf (move-fitness m)
-	    (if (= index (length route)) ;if appending to end of route
-		(distance (node-id (last-node route)) node-ID dist-array)
-		(let ((node-after (node-id (nth index route))))
-		  (-
-		   (+ (distance node-before node-ID dist-array)
-		      (distance node-ID node-after dist-array)) 
-		   (handler-case (distance node-before node-after dist-array)
-		     (same-origin-destination () 0)))))))))
-					     
+            (if (= index (length route)) ;if appending to end of route
+                (distance (node-id (last-node route)) node-ID dist-array)
+                (let ((node-after (node-id (nth index route))))
+                  (-
+                   (+ (distance node-before node-ID dist-array)
+                      (distance node-ID node-after dist-array))
+                   (handler-case (distance node-before node-after dist-array)
+                     (same-origin-destination () 0)))))))))
+
 (defmethod perform-move ((sol problem) (m insertion-move))
   "Performs the <move> on <problem>."
   (with-slots (node-ID vehicle-ID index) m
@@ -69,17 +69,17 @@
 
 (defmethod get-best-insertion-move ((sol problem) (n node))
   (labels ((iter (flt best-move)
-	     (if (null flt) (or best-move (error 'no-feasible-move))
-		 (iter (cdr flt)
-		       (handler-case
-			   (let ((new (get-best-insertion-move-in-vehicle sol
-							       (vehicle-id (car flt))
-							       (node-id n))))
-			     (if (or (null best-move) ;first move
-				     (< (move-fitness new) (move-fitness best-move))) ;better?
-				 new
-				 best-move))
-			 (no-feasible-move () (or best-move)))))))
+             (if (null flt) (or best-move (error 'no-feasible-move))
+                 (iter (cdr flt)
+                       (handler-case
+                           (let ((new (get-best-insertion-move-in-vehicle sol
+                                                                          (vehicle-id (car flt))
+                                                                          (node-id n))))
+                             (if (or (null best-move) ;first move
+                                     (< (move-fitness new) (move-fitness best-move))) ;better?
+                                 new
+                                 best-move))
+                         (no-feasible-move () (or best-move)))))))
     (iter (problem-fleet sol) nil)))
 
 ;; -------------------------
