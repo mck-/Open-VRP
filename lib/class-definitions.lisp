a5ede6b5df99d423d7ca5ad069bfa32b3b79d6b6
Marc Kuo <kuomarc2@gmail.com>
2013-10-09 16:15:17 -0700

    remove iterations as a default slot for base algo class (greedy-best-insertion does not need it)

--- lib/class-definitions.lisp
+++ lib/class-definitions.lisp
@@ -114,6 +114,5 @@
    (best-sol :accessor algo-best-sol :initarg :best-sol :initform nil)
    (best-fitness :accessor algo-best-fitness :initarg :best-fitness :initform nil)
    (best-iteration :accessor algo-best-iteration :initform 0)
-   (current-sol :accessor algo-current-sol :initarg :current-sol :initform nil)
-   (iterations :accessor algo-iterations :initarg :iterations)))
+   (current-sol :accessor algo-current-sol :initarg :current-sol :initform nil)))
    ;; (animatep :accessor algo-animatep :initarg :animatep :initform nil)))
